122f4e3d21103bc1a0a2f9d0a394330903570472
Robin Medina <medina.robin@student.auf.edu.ph>
2026-08-15 16:21:58 +0800

    Add PDF file upload

--- lab3b/uploaded.php
+++ lab3b/uploaded.php
@@ -3,20 +3,14 @@
 $upload_directory = getcwd() . '/uploads/';
 $relative_path = '/uploads/';
 
-// Handle Text File
-$uploaded_text_file = $upload_directory . basename($_FILES['text_file']['name']);
-$temporary_file = $_FILES['text_file']['tmp_name'];
+$uploaded_pdf_file = $upload_directory . basename($_FILES['pdf_file']['name']);
+$temporary_file = $_FILES['pdf_file']['tmp_name'];
 
-if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
-    $text_file_content = file_get_contents($uploaded_text_file, 'r');
+if (move_uploaded_file($temporary_file, $uploaded_pdf_file)) {
+    $pdf_file = $relative_path . basename($_FILES['pdf_file']['name']);
     ?>
-    <textarea cols="70" rows="30"><?php echo $text_file_content; ?></textarea>
+    <iframe src="<?php echo $pdf_file; ?>" width="100%" height="600px"></iframe>
     <?php
 } else {
     echo 'Failed to upload file';
 }
-
-
-echo '<pre>';
-var_dump($_FILES);
-exit;
